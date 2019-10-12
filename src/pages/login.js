103276e60dd0f5ef59615046b1c39efe171f1b9c
Tairine <t.mousinho.de.lima@avanade.com>
2019-10-11 21:21:14 -0300

    Algumas alterações no Login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from '../services/api';
+
 // import { Authenticator } from "aws-amplify-react-native";
 import {
     View,
@@ -8,84 +9,111 @@ import {
     Image,
     StyleSheet,
     TextInput,
-    ImageBackground
+    ImageBackground,
+    KeyboardAvoidingView
 } from "react-native";
-import { thisTypeAnnotation } from "@babel/types";
 
 
+// yarn cache clean
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = { login: '' };
-        this.state = { senha: '' };
+        this.state = { login: Number };
+        this.state = { senha: Number };
+        this.state = { mensagem: '' };
 
     }
     componentDidMount = () => {
-        this.consultaDados();
+        // this.consultaDados();
     }
 
     consultaDados = async () => {
+        var msgerro = '';
         const response = await api.get("/dados");
         const dados = response.data;
 
         dados.map((lista) => {
-            if(lista.cpf === this.state.login && lista.senha === this.state.senha) {
-                this.props.navigation.navigate('Menu');            }
-            
+            if (lista.cpf === this.state.login && lista.senha === this.state.senha) {
+                msgerro = 'Carregando!';
+                this.props.navigation.navigate('Menu');
+
+            } else if (lista.cpf !== this.state.login || lista.senha !== this.state.senha ||
+                this.state.login === undefined || this.state.senha === undefined) {
+                msgerro = ('Dados digitados não conferem!');
+            } 
+
+            this.setState({ mensagem: msgerro, colorAlert: '#900' }).reset();
+
+
         });
     }
 
     render() {
         return (
-
-            <ImageBackground
-                source={require("../image/fundo.png")}
+            <KeyboardAvoidingView
+                keyboardVerticalOffset={100}
                 style={{
                     flex: 1,
-                    width: "100%",
-                    alignItems: "center",
-                    justifyContent: "center"
+                    justifyContent: 'center',
                 }}
-            >
-                <Image style={styles.logo} source={require("../image/Logo_SysEMM.jpg")} />
-                <Text style={styles.loginTitle}>
-                    Sistema de Gerenciamento Escolar.
+                behavior="padding">
+                <ImageBackground
+                    source={require("../image/fundo.png")}
+                    style={{
+                        flex: 1,
+                        width: "100%",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}
+                >
+                    <Image style={styles.logo} source={require("../image/Logo_SysEMM.jpg")} />
+                    <Text style={styles.loginTitle}>
+                        Sistema de Gerenciamento Escolar.
              </Text>
 
-                <View style={styles.container}>
-                    <TextInput
-                        value={this.state.email}
-                        onChangeText={(login) => this.setState({ login })}
-                        placeholder={'Login'}
-                        style={styles.input}
-                        underlineColorAndroid="#ff8c1a"
-                        autoCapitalize="none"
-                        placeholderTextColor="rgba(131,157,182,.7)"
-                    />
-                </View>
+                    <View style={styles.container}>
+                        <TextInput
+                        autoCorrect={false} 
+                            value={this.state.email}
+                            keyboardType="number-pad"
+                            onChangeText={(login) => this.setState({ login })}
+                            placeholder={'Login'}
+                            style={styles.input}
+                            underlineColorAndroid="#ff8c1a"
+                            autoCapitalize="none"
+                            placeholderTextColor="rgba(131,157,182,.7)"
+                        />
+                    </View>
 
-                <View style={styles.container}>
-                    <TextInput
-                        value={this.state.senha}
-                        onChangeText={(senha) => this.setState({ senha })}
-                        placeholder={'Senha'}
-                        secureTextEntry={true}
-                        style={styles.input}
-                        underlineColorAndroid="#ff8c1a"
-                        autoCapitalize="none"
-                        placeholderTextColor="rgba(131,157,182,.7)"
+                    <View style={styles.container}>
+                        <TextInput
+                        autoCorrect={false} 
+                            value={this.state.senha}
+                            onChangeText={(senha) => this.setState({ senha })}
+                            placeholder={'Senha'}
+                            keyboardType="number-pad"
+                            secureTextEntry={true}
+                            style={styles.input}
+                            underlineColorAndroid="#ff8c1a"
+                            autoCapitalize="none"
+                            placeholderTextColor="rgba(131,157,182,.7)"
+                        />
+                    </View>
+                    <Text style={{ fontSize: 15, fontWeight: 'bold', color: '#900' }}>
+                        {this.state.mensagem}
+                    </Text>
+                    <Text></Text>
+                    <Button
+                        title="Acessar"
+                        width="100%"
+                        height={50}
+                        color="#3bd1df"
+                        alignItems="center"
+                        justifyContent="center"
+                        onPress={this.consultaDados}
                     />
-                </View>
-                <Button
-                    title="Acessar"
-                    width="100%"
-                    height={50}
-                    color="#3bd1df"
-                    alignItems="center"
-                    justifyContent="center"
-                    onPress={this.consultaDados}
-                />
-            </ImageBackground>
+                </ImageBackground>
+            </KeyboardAvoidingView>
         );
     }
 }
